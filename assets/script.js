0bb979d6d9d6ca20d63bf4e5d6366be0e0090193
Enzo In <enzit93@hotmail.fr>
2023-12-09 18:34:39 +0100

    fix bulletPoint

--- assets/script.js
+++ assets/script.js
@@ -18,40 +18,44 @@ const slides = [
   },
 ];
 
+//Variables bullet point
+const dotParent = document.querySelector(".dots");
+slides.forEach((slides) => {
+  slides.bulletPoint = document.createElement("span");
+  slides.bulletPoint.classList.add("dot");
+  dotParent.appendChild(slides.bulletPoint);
+});
+
 //Variables arrow
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
-//Variables bullet point
-const dotParent = document.querySelector(".dots");
-const bulletPoint = dotParent.children;
-let dotSelected = document.querySelector(".dot_selected");
+
 //Variables images
 let bannerElement = document.getElementById("banner");
 let paragrapheBanner = bannerElement.querySelector("p");
 let bannerImg = document.querySelector(".banner-img");
 
 let index = 0;
-bulletPoint[index].classList.add("dot_selected");
 
 //Evènement au clique arrow
 arrowLeft.addEventListener("click", function () {
-  bulletPoint[index].classList.remove("dot_selected");
+  slides[index].bulletPoint.classList.remove("dot_selected");
   index--;
   if (index == -1) {
-    index = 3;
+    index = slides.length - 1;
   }
   paragrapheBanner.innerHTML = slides[index].tagLine;
   bannerImg.src = "./assets/images/slideshow/" + slides[index].image;
-  bulletPoint[index].classList.add("dot_selected");
+  slides[index].bulletPoint.classList.add("dot_selected");
 });
 
 arrowRight.addEventListener("click", function () {
-  bulletPoint[index].classList.remove("dot_selected");
+  slides[index].bulletPoint.classList.remove("dot_selected");
   index++;
-  if (index == 4) {
+  if (index == slides.length) {
     index = 0;
   }
   paragrapheBanner.innerHTML = slides[index].tagLine;
   bannerImg.src = "./assets/images/slideshow/" + slides[index].image;
-  bulletPoint[index].classList.add("dot_selected");
+  slides[index].bulletPoint.classList.add("dot_selected");
 });
